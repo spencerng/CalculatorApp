e521603ec9e363be4531dec079ace752504464d0
spencerng <sng1488@gmail.com>
2018-12-27 17:33:22 -0500

    Make initial commit
    Includes essential TODO problems and skeleton code

---
+++ README.md
@@ -0,0 +1,48 @@
+# Calculator App
+
+A simple calculator app and tutorial to teach the basics of Android development and working with a (poorly-written) preexisting codebase.
+
+This tutorial repository was written for FRC Team 25, Raider Robotix, and a more experienced programmer should be assisting others as needed. 
+
+**Prerequisites**
+
+* Solid foundation of Java and object-oriented programming
+* Basic knowledge of XML 
+* Understanding of how Android app source files are structured
+* Debugging skills
+
+**Goals**
+
+* Explore activities, layouts, and manifest files
+* Understand basic linear and relative parameters
+* Create layouts via XML and programatically
+* Continue to develop algorithmic problem-solving skills
+* Understand good material design and Android development practices
+* Learn to expand and modify an existing codebase
+
+More goals may be added as this repository evolves
+
+To begin, [fork this repository](https://guides.github.com/activities/forking/) on GitHub.
+
+Afterward, clone your fork onto your local machine after navigating to your desired directory:
+
+```
+git clone https://github.com/YOUR-USERNAME/CalculatorApp.git
+```
+
+Remember to track changed files and commit changes frequently:
+
+```
+git add -a
+git commit -m "Your commit message here"
+```
+
+Refer to [these guidelines](https://gist.github.com/robertpainsi/b632364184e70900af4ab688decf6f53) when writing commit messages. Keep them short and sweet!
+
+When you're done testing your code and you're sure it works, push it to GitHub (called the remote):
+
+```
+git push origin master
+```
+
+That's it! Good luck!
---
+++ app/src/main/java/org/usfirst/team25/scouting/calculatorapp/MainActivity.java
@@ -0,0 +1,161 @@
+package org.usfirst.team25.scouting.calculatorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // Some variables accessed by all methods in this class
+    private Operation currentOperation = null;
+    private TextView mainDisplay;
+    private RelativeLayout numButtonHolder;
+
+
+    // The first number when completing a computation
+    private double previousNumber;
+
+    // Determines if a second number needs to be inputted (e.g. after an operation is pressed)
+    private boolean inputNewNum = false;
+
+    // Enumerates the different operations so they don't need to be defined by a literal int or string
+    enum Operation {
+        ADDITION, MULTIPLICATION
+    }
+
+    // Our "main" method of the activity. It's called when the activity is first created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        previousNumber = Double.parseDouble(getString(R.string.default_value));
+
+        // Need to link IDs defined in XML to programmatic variables
+        // Note that the R(esource) class is automatically generated
+        mainDisplay = findViewById(R.id.calcMainTextView);
+        numButtonHolder = findViewById(R.id.numberButtonHolder);
+
+        // Creating an array instead of separate variables here makes it easier to perform common functions
+        Button[] operationButtons = {findViewById(R.id.addButton), findViewById(R.id.multiplyButton)};
+
+
+        // TODO Add two more number groups for the numbers 4-9 (inclusive)
+        // There's an easy way to do this, without copying the code two more times!
+
+        // A set of three number buttons
+        LinearLayout numButtonGroup = new LinearLayout(getApplicationContext());
+
+        // Programatically setting the layout parameters (analogous to XML ones) here
+        RelativeLayout.LayoutParams buttonGroupParams =
+                new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        buttonGroupParams.addRule(RelativeLayout.ABOVE, R.id.miscButtonGroup);
+        numButtonGroup.setLayoutParams(buttonGroupParams);
+
+        //How would this help with laying out additional button groups?
+        int currentId = ViewGroup.generateViewId();
+        numButtonGroup.setId(currentId);
+
+        for(int i = 1; i<=3; i++){
+            final Button numButton = new Button(getApplicationContext());
+            numButton.setText(Integer.toString(i));
+
+            // TODO Can you ensure the three buttons have equal widths?
+            // This might require a bit of research on layout parameters...
+
+
+            // This is what triggers an action upon clicking a view
+            // OnClickListeners are very important!
+            numButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(inputNewNum)
+                        mainDisplay.setText(numButton.getText());
+                    else {
+                        String currentNum = (String) mainDisplay.getText();
+                        String newNum = currentNum + numButton.getText();
+                        mainDisplay.setText(formatNumber(Double.parseDouble(newNum)));
+                    }
+                }
+            });
+
+            numButtonGroup.addView(numButton);
+        }
+
+        numButtonHolder.addView(numButtonGroup);
+
+        // Toasts are one among many ways to easily display feedback to users
+        mainDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Text isn't directly editable!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        for(final Button operationButton : operationButtons){
+            operationButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(currentOperation != null)
+                        executeCalculation();
+
+                    // We use casework here and in the implementation of executeCalculation()
+                    // because the functions are essentially the same aside from the operation
+                    if(operationButton.getText().equals(getString(R.string.plus_label))){
+                        currentOperation = Operation.ADDITION;
+                    }
+                    else if(operationButton.getText().equals("x")){
+                        currentOperation = Operation.MULTIPLICATION;
+                    }
+
+                    inputNewNum = true;
+
+                    // TODO There's a slight bug here when "chaining" operations. Can you fix it?
+
+                }
+            });
+        }
+    }
+
+    /** Takes the previous number, selected operation, and current number to compute/display the result
+     */
+    void executeCalculation(){
+        double currentNum = Double.parseDouble((String) mainDisplay.getText());
+        double result = 0;
+
+        switch (currentOperation){
+            case ADDITION:
+                result = previousNumber + currentNum;
+                break;
+            case MULTIPLICATION:
+                result = previousNumber * currentNum;
+                break;
+        }
+
+        mainDisplay.setText(formatNumber(result));
+
+        previousNumber = result;
+
+    }
+
+    // TODO Implement this as you see fit
+    /** Formats numbers into scientific notation or truncates them
+     *  if they are too long, removes leading zeroes, unnecessary decimal places, etc.
+     * @param num
+     * @return num formatted as a readable and parsable string
+     */
+    String formatNumber(double num){
+        if((int)num == num)
+            return Integer.toString((int)num);
+        return Double.toString(num);
+    }
+
+}
